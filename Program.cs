e5f9ab50bf0226a8494ee3682399cdb803a68fec
rfilgueras <68621948+rfilgueras@users.noreply.github.com>
2020-12-14 09:55:04 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BuildAnInsect
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Day 2: Intro to Programming");
+
+            //Getting Input from the user
+            string GameTitle = "Welcome to Ledgends of Insects";   
+            Console.WriteLine("What is your name?");
+            Console.ReadLine();
+            
+
+
+
+            // I am creating my variables;
+            //string myNewString = "";
+            //string myNewString2 = "Hello";
+            //string myNewString3 = "World";
+            //string readingFromTheUser = Console.ReadLine();
+
+            // Combining strings using +
+            //string myCombinedString = myNewString2 + " " + myNewString3;
+
+            //Combine string with String.Format
+            //Its easy to lose track of these numbers
+            //string myOtherCombinedString = //String.Format("{0} {1}", myNewString2, myNewString3);
+
+            //Combine string with Interpolation
+            //Easiest way to combine variables
+            //string combineStringWithInterpolation = $"{myNewString2} {readingFromTheUser}";
+
+
+            //Console.WriteLine(combineStringWithInterpolation); //Prints whatever is inputed by the user
+
+            //Data Types
+            // string myNewString = "Hello World";
+            // int myNewInteger = 100;
+            // bool myNewBool = false;
+            // double myNewDouble = 1.5;
+
+            //Printing to the console output
+            // Console.WriteLine(myNewString);
+            // Console.WriteLine(myNewInteger);
+            // Console.WriteLine(myNewBool);
+            // Console.WriteLine(myNewDouble);
+
+        }
+    }
+}
